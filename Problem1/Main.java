3349df3e265d373a32da6b48b38d9cc79ce4a6e8
14042004-24032006 <poonguzhalic.dev@gmail.com>
2026-07-01 11:53:01 +0530

    1st Problem

--- Problem1/Main.java
+++ Problem1/Main.java
@@ -1,11 +1,35 @@
 package Problem1;
 
-public class Main {
-    public static void main(String[] args) {
-        Product p=new Product("P101","Laptop",1000.0,15);
+import java.util.Scanner;
 
-         p.applyDiscount(10);
-        System.out.println(p.getPrice());
-        System.out.println(p.getStockQuantity());
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Product ID: ");
+        String id = sc.next();
+
+        System.out.print("Enter Product Name: ");
+        String name = sc.next();
+
+        System.out.print("Enter Price: ");
+        double price = sc.nextDouble();
+
+        System.out.print("Enter Stock Quantity: ");
+        int stock = sc.nextInt();
+
+        Product p = new Product(id, name, price, stock);
+
+        System.out.print("Enter Discount Percentage: ");
+        double discount = sc.nextDouble();
+
+        p.applyDiscount(discount);
+
+        System.out.println("Price after discount: " + p.getPrice());
+        System.out.println("Stock Quantity: " + p.getStockQuantity());
+
+        sc.close();
     }
-    }
+}
